7ede0e902aa4cbfc9bb80e092bd570e7c1d2079b
Henrique <rique9906@gmail.com>
2025-03-25 19:49:51 -0300

    amem 🙏

---
+++ 1. Introdução a Programação/Módulos/7 - Dicionários/Aula 2/len_dicionario.py
@@ -0,0 +1,12 @@
+
+dicionario2 = {
+    'nome': 'jack sparrow',
+    'idade': 35,
+    'ocupacao': 'capitao'
+}
+
+print(len(dicionario2))
+
+lista = []
+lista.append(dicionario2)
+print(len(lista))
